44a3e9c564c400b552d3d3c1af750b5286adea8e
Moktan-git <moktanresham3@gmail.com>
2025-04-30 21:49:16 +0545

    first commit

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
 const express = require("express")// express require gareko
 const app = express() // express lai trigger garda
 
+require("./database/connection")
 // const app = require("express")()  --- short-cut-->
 
-app.get("/",(req,res)=>{ // (request,response)
+
+app.get("/books",(req,res)=>{
+
   res.json({
-    name: "Resham Tamang",
-    address:"Sindhupalchok"
+    message: "Books fetched successfully"
   })
 })
-app.get("/about",(req,res)=>{
-  res.send("Bye world")
+
+app.post("/books",(req,res)=>{
+  //logic to add book to database goes here
+  res.json({
+    message: "Book added successfully"
+  })
+})
+
+app.delete("/books/:id",(req,res)=>{
+  // logic to delete book
+  res.json({
+    message: "Book deleted successfully"
+  })
+})
+
+app.patch("/books/:id",(req,res)=>{
+  // logic to update book
+  res.json({
+    message: "Book updated successfully"
+  })
 })
 
 
 
+postgresql://postgres.qbfwlwzfqwucafpqasmf:cXizQMey6DxVyXEN@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres
 
 
-
-app.listen(3000,function(){
-  console.log("server/backend/node project has started at port 3000")
+app.listen(3000,()=>{
+  console.log("project/server/backend/node project has started at port 3000")
 })
-
 
 
---
+++ database/connection.js
@@ -0,0 +1,22 @@
+// hami database connection ko code/logic lekxa
+const {Sequelize,DataTypes} = require("sequelize")  //destructuring of 
+// const sequelize = require("sequelize")
+// const sequelize = sequelize.Sequelize
+// const datatypes = sequelize.DataTypes
+
+
+const sequelize = new Sequelize("postgresql://postgres.qbfwlwzfqwucafpqasmf:cXizQMey6DxVyXEN@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres")
+
+sequelize.authenticate()
+.then(()=>{
+  console.log("Authenticated vayo, connected vayo !!")
+})
+.catch((err)=>{
+  console.log("Error aayo," + err)
+})
+
+const db = {}
+db.Sequelize = Sequelize
+db.sequelize = sequelize
+
+module.exports = db
